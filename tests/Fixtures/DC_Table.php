11dd134e9fd060444e6aea578e9aa90e10e27f41
Kamil Kuzminski <kamil.kuzminski@codefog.pl>
2018-10-03 10:24:23 +0200

    Fix the unit tests

---
+++ tests/Fixtures/DC_Table.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Codefog\MemberExportBundle\Fixtures;
+
+class DC_Table
+{
+    protected $procedure = [];
+
+    protected $values = [];
+
+    protected function showAll()
+    {
+    }
+}
