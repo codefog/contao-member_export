ca79e42795ac9860ef1aeb27c4bb8aac67c70966
Andreas Schempp <andreas.schempp@terminal42.ch>
2025-11-26 10:39:28 +0100

    Add german translations

--- contao/languages/de/tl_member.php
+++ contao/languages/de/tl_member.php
@@ -14,7 +14,7 @@ $GLOBALS['TL_LANG']['tl_member']['export'] = ['Export', 'Daten in eine Datei exp
  * Reference
  */
 $GLOBALS['TL_LANG']['tl_member']['export_description'] = 'Hier können Sie die Mitgliederdaten in eine Datei exportieren. Bitte benutzen Sie das untenstehende Dropdown Menu um das Dateiformat zu wählen. Der Download wird nach Auswahl des Dateiformates automatisch starten.';
-$GLOBALS['TL_LANG']['tl_member']['export_format'] = ['Format'];
+$GLOBALS['TL_LANG']['tl_member']['export_format'] = ['Format', 'Bitte wählen Sie das Format für den Export.'];
 $GLOBALS['TL_LANG']['tl_member']['export_considerFilters'] = ['Listenfilter berücksichtigen', 'Nur die Zeilen exportieren, die in der Listenansicht gefiltert wurden.'];
-$GLOBALS['TL_LANG']['tl_member']['export_headerFields'] = ['Inklusive Zellenüberschrift'];
-$GLOBALS['TL_LANG']['tl_member']['export_raw'] = ['Nur Rohdaten'];
+$GLOBALS['TL_LANG']['tl_member']['export_headerFields'] = ['Inklusive Zellenüberschrift', 'Gibt die Spalten-Namen als erste Zeile im Export aus.'];
+$GLOBALS['TL_LANG']['tl_member']['export_raw'] = ['Nur Rohdaten', 'Die Daten ohne Formattierung exportieren.'];
